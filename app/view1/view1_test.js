948f35de3f172e72b6fbca5a0d75f183de01186f
esiyeenuwa <esi.yeenuwa@gmail.com>
2014-09-20 17:05:39 +0100

    retrives and uses the gender generated by facebook

--- app/view1/view1_test.js
+++ app/view1/view1_test.js
@@ -1,10 +1,25 @@
 'use strict';
 
+
 describe('myApp.view1 module', function() {
 
   beforeEach(module('myApp.view1'));
 
   describe('view1 controller', function(){
+
+    it('should define view1ctrl', inject(function($controller) {
+      //spec body
+	  var mockScope={};
+      var view1Ctrl = $controller('View1Ctrl',{$scope:mockScope},{});
+      expect(view1Ctrl).toBeDefined();
+    }));
+
+  });
+});
+
+  
+  describe('view1 controller', function(){
+  beforeEach(module('myApp.view1'));
 
     it('should ....', inject(function($controller) {
       //spec body
@@ -13,17 +28,23 @@ describe('myApp.view1 module', function() {
     }));
 
   });
-});
 
+  
 
+ 
   describe('view1 controller', function(){
 
+   beforeEach(module('myApp.view1'));
+  
     it('should generate an akan name for a female and with 2014-09-09', inject(function($controller) {
       //spec body
-	  var scope = {},
-      ctrl = $controller('view1Ctrl', {$scope:scope});
+	  var myscope = {
+				birthdate : new Date("2014-9-9"),
+				gender : "Female"},
+      ctrl = $controller('view1Ctrl', {$scope:myscope});
 
-	expect(scope.generateDay()).toEqual("Abena");
+	expect(myscope.generateDay).toBeDefined();
+	expect(myscope.generateDay()).toBe("Abena");
       
     }));
 
